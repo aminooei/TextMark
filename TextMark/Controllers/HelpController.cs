b6ebc6f2708729ec44ed136eb1579d0f37dd7e3e
Amin Minouei <am_minooei@yahoo.com>
2022-02-08 23:24:03 +0330

    Admin help for Projects page done

---
+++ TextMark/Controllers/HelpController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TextMark.Controllers
+{
+    public class Admin_HelpController : Controller
+    {
+        public IActionResult Manage_Projects_Help()
+        {
+            return View();
+        }
+        public IActionResult Manage_Roles_Help()
+        {
+            return View();
+        }
+        public IActionResult Manage_Annotations_Labels_Help()
+        {
+            return View();
+        }
+        public IActionResult Manage_Classifications_Labels_Help()
+        {
+            return View();
+        }
+        public IActionResult Manage_Files_Help()
+        {
+            return View();
+        }
+        public IActionResult Manage_Users_Help()
+        {
+            return View();
+        }
+        public IActionResult Manage_Assigned_Text_Annotations_Help()
+        {
+            return View();
+        }
+        
+        public IActionResult Manage_Assigned_Text_Classifications_Help()
+        {
+            return View();
+        }
+    }
+}
